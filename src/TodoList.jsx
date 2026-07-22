648ae4405a0a5d2ca6f2adc6e086f49496a389de
R-World007 <saimoehein2023@gmail.com>
2026-07-21 23:05:07 -0400

    lesson3

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,15 @@
+import TodoListItem from './TodoListItem';
 
+function TodoList({todoList}){
 
-function TodoList(){
-    const todoList = [
-    {id: 1, title: 'review resources'},
-    {id: 2, title: 'take notes'},
-    {id: 3, title: 'code out app'},
-]
     return(   
         <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
+          {todoList.map((todo) => (
+  <TodoListItem
+    key={todo.id}
+    todo={todo}
+  />
+))}
       </ul>
     );
 }
