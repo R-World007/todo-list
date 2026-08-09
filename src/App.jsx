54967316c70b34baa38d2b78d28f6b6d2ed27373
R-World007 <saimoehein2023@gmail.com>
2026-08-08 21:04:53 -0400

    week5

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,25 @@ function App() {
     const newTodo = {
       id: Date.now(),
       title: todoTitle,
+      isCompleted: false,
     };
 
     setTodoList((previous) => [newTodo, ...previous]);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodoList = todoList.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: true } : todo,
+    );
+
+    setTodoList(updatedTodoList);
   };
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
